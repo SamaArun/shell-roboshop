bfc4bb7bd30fd9ed69162a0ff2571ddb027a33c9
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-01 23:56:57 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,16 +2,35 @@
 
 AMI_ID="ami-0532be01f26a3de55"
 SG_ID="sg-03d101dcaa6a55f3d"
-INSTANCES=("mangodb" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for instance in ${INSTANCES[@]}
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+for instance in "${INSTANCES[@]}"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0532be01f26a3de55 --instance-type t3.micro --security-group-ids sg-03d101dcaa6a55f3d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend"]
-    then
-        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+    echo "Launching instance: $instance"
+
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query "Instances[0].InstanceId" \
+        --output text)
+
+    echo "Waiting for instance $INSTANCE_ID to be running..."
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query "Reservations[0].Instances[0].PrivateIpAddress" \
+            --output text)
     else
-        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query "Reservations[0].Instances[0].PublicIpAddress" \
+            --output text)
     fi
+
     echo "$instance IP address: $IP"
 done
