2501d56918e10a44aea61c3c24174e1a0612fdd8
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-02 15:12:00 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -2,8 +2,9 @@
 
 AMI_ID="ami-0532be01f26a3de55"
 SG_ID="sg-03d101dcaa6a55f3d"
-
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="dsjnjsndnd"
+# DOMAIN_NAME="arun.in"
 
 for instance in "${INSTANCES[@]}"
 do
@@ -20,7 +21,8 @@ do
     echo "Waiting for instance $INSTANCE_ID to be running..."
     aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
 
-    if [ "$instance" != "frontend" ]; then
+    if [ "$instance" != "frontend" ];
+    then
         IP=$(aws ec2 describe-instances \
             --instance-ids "$INSTANCE_ID" \
             --query "Reservations[0].Instances[0].PrivateIpAddress" \
@@ -33,4 +35,29 @@ do
     fi
 
     echo "$instance IP address: $IP"
+
+    # aws route53 change-resource-record-sets \
+    # --hosted-zone-id $ZONE_ID \
+    # --change-batch '
+    #  {
+    #     "Comment": "Creating or Updating a record set for cognito endpoint",
+    #     "Changes": [{
+    #         "Action": "UPSERT",
+    #         "ResourceRecordSet": {
+    #             "Name": "'$instance'.'$DOMAIN_NAME'",
+    #             "Type": "A",
+    #             "TTL": 1,
+    #             "ResourceRecords": [{
+    #                 "Value": "'$IP'"
+    #             }]
+    #         }
+    #     }]
+    #  }'
 done
+
+
+
+
+
+
+
